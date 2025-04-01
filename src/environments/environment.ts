7da8b922054e96f70dd26a2340a9941f050f376c
durgesh-firake09 <firake.durgesh876@gmail.com>
2025-04-01 15:36:49 +0530

    added ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    customerUrl:"http://localhost:1000",
-    adminUrl:"http://localhost:1002"
+    customerUrl:"http://172.16.16.142:1000",
+    adminUrl:"http://172.16.16.142:1002"
 };
