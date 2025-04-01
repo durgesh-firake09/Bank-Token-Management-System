c18028fc956f737a641152b74a463271e729a6f0
durgesh-firake09 <firake.durgesh876@gmail.com>
2025-04-01 15:42:41 +0530

    added ips

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,9 @@
 export const environment = {
-    customerUrl:"http://172.16.16.142:1000",
-    adminUrl:"http://172.16.16.142:1002"
+    customerUrl:"http://localhost:1000",
+    adminUrl:"http://localhost:1002"
 };
+
+// export const environment = {
+//     customerUrl:"http://172.16.16.142:1000",
+//     adminUrl:"http://172.16.16.142:1002"
+// };
